8ec394b26802709d7e79b55ec7a2b73ec7476fc4
Mendy <emkay5771@outlook.com>
2023-05-22 17:12:54 -0400

    disable dev shm usage

--- dvarstream.py
+++ dvarstream.py
@@ -24,6 +24,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 options.add_experimental_option('prefs', {
     "download.default_directory": "~/ccscraper/dvarmalchus",
