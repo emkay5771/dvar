9a9f0e1cb5de90566cf5e9f8bab3369f1ed88afd
Mendy <emkay5771@outlook.com>
2023-05-23 17:15:25 -0400

    add debug statement

--- dvarstream.py
+++ dvarstream.py
@@ -417,15 +417,15 @@ def dynamicmake(dow, optconv, opt, source, session):
                             if "ברכת הפטורה" in text or "xtd enk dxhtdd renyl" in text:
                                 doc_out.insert_pdf(doc, from_page=page_num, to_page=page_num_end) #type: ignore
                                 continue
-            if q == 'Rambam (3)-Bilingual':
-                st.write("Appending Rambam")
+            '''if q == 'Rambam (3)-Bilingual':
+                #st.write("Appending Rambam")
                 doc_out.insert_pdf(fitz.open(f"Rambam{session}.pdf")) 
                 print("Appended")
-                continue
+                continue'''
             
             if q == 'Hayom Yom':
                 print("Hayom Yom found")
-                st.write("Appending Hayom Yom")
+                #st.write("Appending Hayom Yom")
                 doc_out.insert_pdf(fitz.open(f"Hayom{session}.pdf")) 
                 print("Appended")
                 continue
