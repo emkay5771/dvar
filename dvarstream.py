e7b6320c7580130db5bc84ff84247a55b035d953
Mendy Kay <52011243+emkay5771@users.noreply.github.com>
2024-01-17 23:03:17 +0000

    [FIX] update xpath for dvarget

--- dvarstream.py
+++ dvarstream.py
@@ -45,6 +45,7 @@ def dvarget(session2): # attempts to retrieve dvar malchus pdf
     driver = webdriver.Chrome(options=options)
     driver.get("https://dvarmalchus.org")
     xpaths = [
+        "/html/body/div[1]/section[2]/div[3]/div/div/div[4]/div/div/section/section/div/div/div/div"
         "/html/body/div[1]/section[2]/div[3]/div/div/div[4]/div/div/section/section/div/div/div/div/div/div",
         '/html/body/div[1]/section[2]/div[3]/div/div/div[3]/div/div/a',
         '/html/body/div[1]/section[2]/div[3]/div/div/div[4]/div/div/section/section/div/div/div/div[1]/div/div/a',
